fcf60fb1418d025f4761f2393b16be4c75727fb2
Arnav Thakrar <arnav.thakrar@uconn.edu>
2026-03-28 19:28:00 -0400

    Command.com just compiled successfully
    01:47:42 In #OSdev 01

--- osapi/osapi.c
+++ osapi/osapi.c
@@ -1,2 +1,34 @@
 /* osapi.c */
+#define Library
 #include "osapi.h"
+
+private fd fds[256];
+
+private bool isopen(fd file) {
+    signed int posixfd;
+    struct stat _;
+
+    if (file < 2)
+        return false;
+
+    posixfd = getposixfd(file);
+    if (posixfd == -1)
+        return false;
+
+    if (fstat(posixfd, &_) == -1)
+        return false;
+
+    return true;
+
+}
+
+public bool load(fd file, int8 c) {
+    if (file > 1) // not stdin or stdout
+        if (!isopen(file))
+            reterr(ErrBadFD);
+    return false;
+}
+
+public int8 store(fd file) {
+    return 0;
+}
